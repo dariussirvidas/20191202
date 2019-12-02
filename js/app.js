4a1c56533f8626f6639bb3b28868f5d7964e2f1a
dariussirvidas <dsirvidas@yahoo.com>
2019-12-02 11:24:54 +0200

    Article fono, teksto ir teksto spalvos keitimas Js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,25 @@
 "use strict";
 const section = document.querySelector("section");
+const articles = document.querySelectorAll("article");
 const btn = document.querySelector("button");
 const inputColor = document.querySelector("input[placeholder = \"Fono spalva\"]");
 const inputFontSize = document.querySelector("input[placeholder = \"Teksto dydis\"]");
 const inputFontColor = document.querySelector("input[placeholder = \"Teksto spalva\"]");
 
-btn.addEventListener("click", keiskFona);
+console.log(section.style.backgroundColor);
 
-function keiskFona() {
+btn.addEventListener("click", keiskArticle);
+
+/*function keiskFona() {
     section.style.backgroundColor = inputColor.value;
     section.style.fontSize = inputFontSize.value + "px";
     section.style.color = inputFontColor.value;
+}*/
+
+function keiskArticle() {
+    for(let article of articles) {
+        article.style.backgroundColor = inputColor.value;
+        article.style.fontSize = inputFontSize.value + "px";
+        article.style.color = inputFontColor.value;
+    }
 }
